22e50ef54a14855c96204bdc48056fa50f531563
heseltime <jack.heseltine@gmail.com>
2021-02-11 09:35:58 +0100

    Update settings.py
    400 error bugfix: allowed host flexibility

--- heroku_django_vue/settings.py
+++ heroku_django_vue/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://heroku-django-vue.herokuapp.com/','localhost']
+ALLOWED_HOSTS = ['.herokuapp.com','localhost']
 
 
 # Application definition
